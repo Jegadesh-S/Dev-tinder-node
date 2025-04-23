219a64303aac47c0459fa287776e99c7e0e290a9
Jegadesh S <59415549+Jegadesh-S@users.noreply.github.com>
2025-04-23 23:24:01 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const PORT = 3000;
+
+const app = express();
+
+// implementing API routing
+// handling API incoming request 
+app.use("/", (req, res)=>{
+    res.send("Welcome to DevTinder Service App")
+});
+
+
+app.listen(PORT,()=>{
+    console.log(`DevTinder serice running with port ${PORT}`)
+});
 
